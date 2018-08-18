6182184b05b953cf7760462cb40bc981c0e06205
ZiraelS <zepherXD@gmail.com>
2018-08-17 19:31:16 -0600

    DB Scripts

---
+++ DB/Query/SQLQuery DB LookUp Country.sql
@@ -0,0 +1,10 @@
+INSERT INTO [dbo].[Country]
+           ([CountryID]
+           ,[CountryName])
+     VALUES
+           (1,'India')
+		 , (2,'Nepal')
+		 , (3,'Thailand')
+		 , (4,'Dubai')
+		 , (5,'Australia')
+		 , (6,'USA')
---
+++ DB/Query/SQLQuery DB LookUp CustomerOrigin.sql
@@ -0,0 +1,21 @@
+INSERT INTO [dbo].[CustomerOrigin]
+           ([SCustomerID]
+           ,[SCustomerName]
+           ,[ScustomerAmount]
+           ,[SCustomerAddress]
+           ,[SCustomerCountry]
+           ,[SCustomerIsActive])
+     VALUES
+           (1001,'Sham',5655,'Kurla Naka','India','Y')
+		, (1002,'Shiv',3274,'Milind Link Road','USA','Y')
+		, (1003,'Khadak',4793,'Kathmandu Road','Neapl','N')
+		, (1004,'Robin',1793,'Makers Road','Thailand','Y')
+		, (1005,'Gary',4657,'MBR City','Dubai','N')
+		, (1006,'Allwyn',4641,'Wellington Street','Australia','N')
+		, (1007,'Fred',3233,'Managua','Nicaragua','Y')
+		, (1008,'Sam',8942,'San Marino','San Marino','N')
+		, (1009,'Tom',4456,'Rome','Italy','Y')
+		, (1010,'Wally',1211,'Tokyo','Japan','N')
+
+
+
